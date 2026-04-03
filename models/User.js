e84d11922e02fa969d15affba5ea071bf3b71d1b
FotiosAp <fotisapostolou90@gmail.com>
2026-04-03 16:23:49 +0300

    refactor 0.0

--- models/User.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    role: { type: String, enum: ['admin', 'manager'], default: 'manager' }
-});
-
-export const User = mongoose.model('User', userSchema);
